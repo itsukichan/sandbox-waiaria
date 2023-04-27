4f140891ac11e087458180fe99ad53922962ce72
itsukichan <kou05ituki09design@gmail.com>
2023-04-27 21:50:31 +0900

    add webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./style.scss";
 import Accordion from "./components/accordion/_accordion.js";
 
 new Accordion(".js-accordion");
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
