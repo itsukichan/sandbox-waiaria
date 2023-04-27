b2ddbc8bdff2482a6a82f17fa733535ce4c82224
itsukichan <kou05ituki09design@gmail.com>
2023-04-27 22:53:50 +0900

    add: hot reload

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "development",
@@ -11,5 +12,19 @@ module.exports = merge(common, {
     hot: true,
     open: true,
     port: 3000,
+    watchFiles: ["src/**/*.{js,scss,html}"],
+    client: {
+      logging: "info",
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
 });
